5905a94b166ab4355208b1a457128643ed4633f2
lazyfortress <jander31@terpmail.umd.edu>
2025-05-18 20:17:20 -0400

    Formatting

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function renderChart(canvasId, labels, data, label, color = "green") {
                         label: function (context) {
                             const label = context.dataset.label;
                             const value = context.raw;
-                            if (label.includes("REIT")) return `${label}: $${value.toFixed(2)}`;
+                            if (label.includes("Price")) return `${label}: $${value.toFixed(2)}`;
                             if (label.includes("Fed Funds")) return `${label}: ${value.toFixed(2)}%`;
                             if (label.includes("Unemployment")) return `${label}: ${value.toFixed(1)}%`;
                             if (label.includes("CPI")) return `${label}: ${value.toFixed(3)}`;
